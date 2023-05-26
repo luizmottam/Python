0ae7486463c98968ca5239937a875305f85abee6
unknown <2212130077@academico.iesb.br>
2023-05-26 09:15:34 -0300

    Funções de leitura adicionadas - V2

--- analisa_duplicidade.py
+++ analisa_duplicidade.py
@@ -1,10 +1,31 @@
 import pandas as pd
-import os
 
-df = pd.read_csv('Produtos.csv',  delimiter=';') # Nome do arquivo
+df = pd.read_excel('Teste.xlsx') # Nome do arquivo
 
-num_colunas = df.shape[1] # Quantidades de colunas
-primeira_coluna = df.iloc[:, 0] # Pegar coluna sem o nome
-primeiras_duas_colunas = df.iloc[:, [0, 1]] # Pegar mais de uma coluna e separar elas
+#primeira_coluna = df.iloc[:, 0] # Pegar coluna sem o nome
+#primeiras_duas_colunas = df.iloc[:, [0, 1]] # Pegar mais de uma coluna e separar elas
 
-print(num_colunas)
+def le_coluna_por_coluna(df):
+
+  num_colunas = df.shape[1] # Quantidades de colunas
+  
+  for i in range(num_colunas):
+    coluna_em_analise = df.iloc[:, i]
+    tem_duplicatas = coluna_em_analise.duplicated().any()
+  
+    if tem_duplicatas:
+      print(f"Na coluna {df.columns[i]} existem duplicatas")
+    else:
+      print(f"Na coluna {df.columns[i]} não tem duplicatas")
+
+
+def le_linha(df):
+  # Verificar as linhas duplicadas
+  linhas_duplicadas = df.duplicated()
+  
+  # Exibir as linhas duplicadas
+  linhas_duplicadas = df[linhas_duplicadas]
+  print(linhas_duplicadas)
+
+
+le_linha(df)
